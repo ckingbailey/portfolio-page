3a4db904820849f27a9153b4592c269a1abb74e9
ckingbailey <ckingbailey@gmail.com>
2018-01-30 13:40:28 -0800

    close projects on btn click

--- new-version/script.js
+++ new-version/script.js
@@ -1,30 +1,35 @@
 const onProjectBtnClick = (function() {
   const pd = document.getElementById("projectDisplayArea");
   const img = pd.children.projectView;
-  const projects = ["bisbeevacationrental", "turtrello"];
 
   return function handleProjectBtnClick(event) {
     event.preventDefault();
     // get the <a> tag with the data attr on it
     const target = event.target;
-    const btn = target.dataset.projectnum ?
+    const btn = target.dataset.targetfor ?
       target :
       target.tagName === "IMG" ?
         target.parentElement :
         target.tagName === "LI" ?
-          target.querySelector("[data-projectnum]") :
+          target.querySelector("[data-targetfor]") :
           null;
-    const curProject = btn && btn.dataset.projectnum;
+    const targetProject = btn && btn.dataset.targetfor;
+    const curProject = pd.children.projectView.dataset && pd.children.projectView.dataset.displayfor;
     if (Array.from(pd.classList).includes("empty-display") ||
-      +curProject !== projects.indexOf(img.src)) {
+      targetProject !== curProject) {
       // if display is empty
       // or if btn for project other than currently loaded project is clicked
       // load project view
       pd.classList.remove("empty-display");
-      console.log(curProject, img.src);
-      img.setAttribute("src", `img/${projects[curProject]}.png`);
+      console.log(btn, targetProject);
+      img.setAttribute("src", `img/${targetProject}.png`);
+      img.setAttribute("data-displayfor", targetProject);
     }
     // if btn for currently loaded project is clicked, close it
-    else pd.classList.add("empty-display");
+    else {
+      pd.classList.add("empty-display");
+      img.setAttribute("src", "");
+      img.setAttribute("data-displayfor", "");
+    }
   }
 })();
